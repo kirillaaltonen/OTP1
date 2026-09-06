3c933ce1f82d98d428a7f6eceb7d17971a6825e4
alokas <kirill.aaltonen@metropolia.fi>
2026-09-06 23:34:44 +0300

    Final Commit, test passed

---
+++ src/test/java/TemperatureConverterTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TemperatureConverterTest {
+
+    TemperatureConverter converter = new TemperatureConverter();
+
+    @Test
+    void fahrenheitToCelsius() {
+        assertEquals(0, converter.fahrenheitToCelsius(32));
+        assertEquals(100, converter.fahrenheitToCelsius(212));
+    }
+
+    @Test
+    void celsiusToFahrenheit() {
+        assertEquals(32, converter.celsiusToFahrenheit(0));
+        assertEquals(212, converter.celsiusToFahrenheit(100));
+    }
+
+    @Test
+    void isExtremeTemperature() {
+        assertTrue(converter.isExtremeTemperature(-41));
+        assertTrue(converter.isExtremeTemperature(51));
+        assertFalse(converter.isExtremeTemperature(25));
+    }
+}
